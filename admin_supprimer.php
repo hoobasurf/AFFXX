00416b128395a35a91648a44759bb3c3d1a14081
hoobasurf <brice.paneela@icloud.com>
2025-06-17 20:11:43 +0200

    Create admin_supprimer.php

---
+++ admin_supprimer.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+if (!isset($_SESSION['admin'])) {
+    header("Location: admin_connexion.html");
+    exit;
+}
+
+if (isset($_POST['index'])) {
+    $index = (int) $_POST['index'];
+    $utilisateurs = file("data/utilisateurs_validés.csv", FILE_IGNORE_NEW_LINES);
+    if (isset($utilisateurs[$index])) {
+        unset($utilisateurs[$index]);
+        file_put_contents("data/utilisateurs_validés.csv", implode("\n", $utilisateurs));
+    }
+}
+
+header("Location: admin_panel.php");
+exit;
