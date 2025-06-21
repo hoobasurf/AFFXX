1895365e4de00005c1c245a4e3c2e6439bdfc8fe
hoobasurf <brice.paneela@icloud.com>
2025-06-21 19:24:00 +0200

    Delete send_validation.php

--- send_validation.php
+++
@@ -1,7 +0,0 @@
-<?php
-$to = "ton@email.com"; // ← Mets ton email ici
-$subject = "✅ Nouvelle inscription validée - AFFINIX";
-$message = "Nom : $nom\nEmail : $email\nSalles : $salles\n";
-$headers = "From: no-reply@affinix.com";
-
-mail($to, $subject, $message, $headers);
